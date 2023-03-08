0b6a4291726b9f03b14cebb9a8b32d7292be2949
jasper <lambokxk@gmail.com>
2023-03-07 17:58:15 -0800

    ui updates

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -2,8 +2,12 @@ import React from 'react';
 
 const Login = () => {
     return (
-        <div>
-            <h1>Login</h1>
+        <div className="login-container">
+            <div className="login-forms">
+                <div className="login-forms login">
+                    <span className="title">Login</span>
+                </div>
+            </div>
         </div>
     );
 };
